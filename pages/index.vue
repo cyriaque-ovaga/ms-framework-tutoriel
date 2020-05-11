2284b0b38734722cb5de7e1d0396f60a5e187c53
cyriaque ovaga <ghislain.ovaga@gmail.com>
2020-05-11 21:37:40 +0100

    #1 installation de nuxt et le template

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h2 align="center">A la découvert de MS-Framework PHP</h2>
+    <div class="embed-responsive embed-responsive-16by9">
+      <iframe
+        class="embed-responsive-item"
+        src="https://www.youtube.com/embed/8EVVKDo4jJM?rel=0"
+        allowfullscreen
+      ></iframe>
+    </div>
+    <h3>Plan de formation</h3>
+
+    <table class="table table-light">
+      <thead class="thead-light">
+        <tr>
+          <th>Module</th>
+          <th>Durée</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <nuxt-link to="/">Découverte</nuxt-link>
+          </td>
+          <td>5 minutes</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+
+export default {
+  components: {}
+};
+</script>
